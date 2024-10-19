b8a98561bb965e684912d15c6a851ff8ac63ff1c
Val <darsuev@gmail.com>
2024-10-19 14:23:53 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,41 +1,41 @@
 #include <fcntl.h>
 #include <stdio.h>
-#include <unistd.h>
 #include "get_next_line.h"
 
-// Без бонуса
-int main(int ac, char **av) {
-    int fd;
-    char *line;
-    int count = 0; // номера строк
+// NO BONUS MAIN
+int     main(int ac, char **av)
+{
+        int             fd;
+        char    *line;
+        int             count_lines;
 
-    if (ac == 1) {
-        fd = 0; // стандартный ввод (stdin)
-        printf("Reading from stdin (press Ctrl+D to stop):\n");
-    } else {
-        fd = open(av[1], O_RDONLY);
-        if (fd == -1) {
-            write(1, "Cannot read file.\n", 18);
-            return (1);
+        count_lines = 0;
+        if (ac == 1)
+        {
+                fd = 0;
+                printf("Reading from stdin (press Ctrl + D to stop):\n");
         }
-    }
-
-    // Цикл для чтения строк
-    while (1) {
-        line = get_next_line(fd);
-        if (line == NULL) {
-            break;
+        else
+        {
+                fd = open(av[1], O_RDONLY);
+                if (fd == -1)
+                {
+                        printf("Cannot read file.\n");
+                        return (1);
+                }
         }
-        count++;
-        printf("LINE [%d] =\n%s\n", count, line);
-        printf("------------\n");
-        free(line); // Освобождение памяти
-    }
+        while (1)
+        {
+                line = get_next_line(fd);
+                if (line == NULL)
+                        break;
+                count_lines++;
+                printf("LINE [%d] =%s\n", count_lines, line);
+                free(line);
+        }
+        if (ac > 1)
+                close(fd);
 
-    // Закрытие файла, если ввод был не из stdin
-    if (ac > 1) {
-        close(fd);
-    }
-    
-    return (0);
+        return (0);
 }
+
